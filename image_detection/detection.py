34b5618d2deb1d15279891aceacdad59781c8998
CasperWong-jpg <casper.wong.001@hotmail.com>
2022-09-19 18:23:19 -0400

    base code to read an image

---
+++ image_detection/detection.py
@@ -0,0 +1,26 @@
+# Importing the OpenCV library
+import cv2
+import numpy as np
+
+# define a video capture object
+cap = cv2.VideoCapture(1)
+
+assert (cap.isOpened())
+
+# Capture frame-by-frame
+ret, frame = cap.read()
+if ret:
+    # Display the resulting frame
+    cv2.imshow('Frame', frame)
+
+    # waits for user to press any key
+    # (this is necessary to avoid Python kernel form crashing)
+    cv2.waitKey(0)
+
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
