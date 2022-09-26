19361cd84b3cf7b758572f9f929852e71c43a404
CasperWong-jpg <casper.wong.001@hotmail.com>
2022-09-26 17:24:37 -0400

    added some test values

--- image_detection/detection.py
+++ image_detection/detection.py
@@ -9,12 +9,18 @@ assert (cap.isOpened())
 
 # Capture frame-by-frame
 ret, frame = cap.read()
+B, G, R = cv2.split(frame)
 if ret:
     # Display the resulting frame
     cv2.imshow('Frame', frame)
-
     # waits for user to press any key
-    # (this is necessary to avoid Python kernel form crashing)
+    # (this is necessary to avoid Python kernel from crashing)
+    cv2.waitKey(0)
+
+    cv2.imshow('Red', R)
+    cv2.waitKey(0)
+
+    cv2.imshow('Green', G)
     cv2.waitKey(0)
 
 
