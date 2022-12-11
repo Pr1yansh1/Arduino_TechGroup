e44f380f449df6591e21bcb47109a69609484cd7
CasperWong-jpg <casper.wong.001@hotmail.com>
2022-12-10 22:21:34 -0500

    Refactored code, started main()

---
+++ image_detection/imageDetection.py
@@ -0,0 +1,42 @@
+# Importing the OpenCV library
+import cv2
+import numpy as np
+
+"""
+Capture an image from webcam and return frame (NumPy array)
+"""
+def captureImage():
+    # define a video capture object
+    cap = cv2.VideoCapture(0)
+
+    assert (cap.isOpened())
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # When everything done, release
+    # the video capture object
+    cap.release()
+
+    return frame
+
+
+"""
+Given an image, return filtered (only green) parts
+Code from https://pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+"""
+def detectColor(image):
+    boundaries = [([0, 0, 0], [100, 255, 75])]
+
+    (lower, upper) = boundaries[0]
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
+
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask = mask)
+
+    cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
+
+    return output
+
--- image_detection/main.py
+++ image_detection/main.py
@@ -1,23 +1,57 @@
 import RPi.GPIO as GPIO
 import time
-import cv2
+from enum import Enum
+
+import imageDetection as img
+
+# Pin setup for a Raspberry Pi 3 A
+class pins(Enum):
+    moveLeftPin = 11  # GPIO 17: Tell Arduino to move left
+    waterPin = 13  # GPIO 27: Tell Arduino to water
+    moveRightPin = 15  # GPIO 22: Tell Arduino to move right
+
+    arduinoReadyPin = 16  # GPIO 23: Arduino is ready to take next decision
+
 
 """
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
-
-for i in range(5):
-    GPIO.output(7, True)
-    time.sleep(1)
-    GPIO.output(7, False)
-    time.sleep(1)
-GPIO.cleanup()
+Initialize pins.
 """
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    # The following are all active high inputs and outputs
+    GPIO.setup(pins.moveLeftPin, GPIO.OUT)
+    GPIO.setup(pins.waterPin, GPIO.OUT)
+    GPIO.setup(pins.moveRightPin, GPIO.OUT)
+    GPIO.setup(pins.arduinoReadyPin, GPIO.IN) 
 
-cap = cv2.VideoCapture(0)
-assert(cap.isOpened())
-ret, frame = cap.read()
-if ret:
-    cv2.imshow('BlairShco', frame)
-    cv2.waitKey(0)
 
+def sendCommand(pin):
+    assert(pin in pins)
+
+    GPIO.output(pin, True)
+    time.sleep(1)
+    GPIO.output(pin, False)
+
+"""
+The brain of the algorithm. Loops infinitely until powered off.
+"""
+def main():
+    init()
+
+    try: 
+        while True:
+            if GPIO.input(pins.arduinoReadyPin):
+                filtered_img = img.detectColor(img.captureImage())
+
+                sendCommand(pins.moveLeftPin)
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("Expected program termination")
+    except:
+        print("Unexpected program termination")
+    finally:
+        GPIO.cleanup()  # Changes all pins back to inputs
+
+
+main()
